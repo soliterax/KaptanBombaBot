930518ee1e619d5a5c6b90ea0b1070b68575a689
Anar Abdullayev <anaryek@hotmail.com.tr>
2017-11-04 15:13:29 +0400

    Update music.js

--- music.js
+++ music.js
@@ -140,7 +140,7 @@ function add_to_queue(strID) {
 }
 
 function search_video(query, callback) {
-	request("https:\\www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURIComponent(query) + "&key=" + yt_api_key, function(error, response, body) {
+	request("https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURIComponent(query) + "&key=" + yt_api_key, function(error, response, body) {
         var json = JSON.parse(body);
         callback(json.items[0].id.videoID);
     });
